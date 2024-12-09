09aedae25577e20d1c1d868a2a701146daffa985
Shoaibtahir1 <shoaibtahir90@gmail.com>
2024-12-10 00:50:06 +0500

    hackton 5

---
+++ src/app/page.tsx
@@ -0,0 +1,11 @@
+import "./globals.css";
+import Hero from './components/Hero';      // assuming you have a Hero component
+
+export default function Home() {
+  return (
+    <>
+    
+    <Hero />
+    </>
+  );
+}
